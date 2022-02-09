7b71a1d7b91142e6891dcba2fd275801df317582
specit <andriik00@gmail.com>
2022-02-09 14:42:00 +0300

    find and test file

--- test_004.sh
+++ test_004.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [[ $(find /mnt/c/Users/seligenenko/port/ -type f -name 'gridnine*' -cmin +3 | wc -l) > 0 ]]
+if [[ $(find /root/dfgdfg/ -type f -name 'gridnine*' -cmin +5 | wc -l) > 0 ]]
 then
  echo 'find files'
- if [[ $(find /mnt/c/Users/seligenenko/port/ -type f -name 'gridnine*' -perm 0644) ]]
+ if [[ $(find /root/dfgdfg/ -type f -name 'gridnine*' -perm 0644) ]]
  then
-  chmod 666 /mnt/c/Users/seligenenko/port/gridnine*
+  chmod 666 /root/dfgdfg/gridnine*
  else
   echo -e 'Subject:Test File\n\nФайлы найдены\nФайлы изменены\nФайлы не загрузились' | sendmail -v 107@maverik.ru
  fi
